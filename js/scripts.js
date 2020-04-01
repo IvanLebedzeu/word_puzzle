0d46d0a68dec601e623c0649143ebc5b2704399d
Ivan Lebedzeu <ivan.lebedzeu@gmail.com>
2020-03-31 18:17:07 -0700

    shows the result using 2 loops, 1 is nested to another one

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,34 @@ $(document).ready(function() {
     event.preventDefault();
 
     var userSentence = $("input#sentence").val();
-    var sentenceArray = userSentence.split(' ');
+    var sentenceArray = userSentence.split("");
 
     var vowels = ['a', 'e', 'i', 'o', 'u'];
 
     var stringWithoutVowels = [];
 
-   
+    for (var index = 0; index < sentenceArray.length; index++)
+    {
+      for (var index1 = 0; index1 < vowels.length; index1++) {
+        if (sentenceArray[index] === vowels[index1]) {
+          sentenceArray[index] = "-";
+        }
+      } sentenceArray[index];
+    }
 
-    $("#output").text(sentenceArray);
+  //   for (var index = 0; index < sentenceArray.length; index++) {
+  //     if(sentenceArray[index] === "a" || sentenceArray[index] === "e" || sentenceArray[index] === "i" || sentenceArray[index] === "o" || sentenceArray[index] === "u") {
+  //       sentenceArray[index] = "-";
+
+        
+        
+      
+  //   }
+  // }
+
+    var stringWithoutVowels = sentenceArray.join(' ');
+
+    $("#output").text(stringWithoutVowels);
 
   })
 })
